01e71880f3fc01bee0f7cda2e3310599a6df8c0f
habeeb-cod3r <adeyinkahabeeb2010@gmail.com>
2022-09-20 17:20:37 +0100

    Display Product

---
+++ ShopOnline.Web/Services/Contracts/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using ShopOnline.Models.DTOs;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services.Contracts
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient=httpClient;
+        }
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var products = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+                return products;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
